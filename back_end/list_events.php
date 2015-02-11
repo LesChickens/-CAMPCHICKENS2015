283b1081773f64a63ec5ef0f0a294ad47753fc79
Léo Davesne <leo@davesne.net>
2015-02-10 23:28:24 -0500

    API de base

--- back_end/list_events.php
+++ back_end/list_events.php
@@ -15,11 +15,26 @@
 	$select_events = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
 	$results = $connection -> query($select_events);
 
+	$arr = array();
+	
 	if ($results -> num_rows > 0) {
 		// Output data of each row
 		while($row = $results -> fetch_assoc()) {
 			echo "id: " . $row["id"] . " title: " . $row["title"] . " short_description: " . $row["short_description"] . " long_description: " . $row["long_description"] . " start_date: " . $row["start_date"] . " end_date: " . $row["end_date"] . " author: " . $row["author"] . " category: " . $row["category"] . " level " . $row["level"] . "<br />";
+			
+			$row_array['id'] = $row['id'];
+			$row_array['short_description'] = $row['short_description'];
+			//$row_array['long_description'] = $row['long_description'];
+			//$row_array['start_date'] = //$row['start_date'];
+			//$row_array['end_date'] = $row['end_date'];
+			//$row_array['author'] = $row['author'];
+			//$row_array['category'] = $row['category'];
+			//$row_array['level'] = $row['level'];
+			
+			array_push($arr,$row_array);
 		}
+		
+		echo json_encode($arr);
 	} else {
 		echo "0 results";
 	}
