95497a7f6c0d1691619b22214bc31d3f6d19e4c8
Léo Davesne <leo@davesne.net>
2015-02-10 23:08:24 -0500

    Formatage

--- back_end/list_events.php
+++ back_end/list_events.php
@@ -5,24 +5,24 @@
 	$db_name = "acc";
 
 	// Create connection
-	$conn = new mysqli($server_name, $username, $password, $db_name);
+	$connection = new mysqli($server_name, $username, $password, $db_name);
 	
 	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
+	if ($connection -> connect_error) {
+		die("Connection failed: " . $connection -> connect_error);
 	} 
 
-	$sql = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
-	$result = $conn->query($sql);
+	$select_events = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
+	$results = $connection -> query($select_events);
 
-	if ($result->num_rows > 0) {
+	if ($results -> num_rows > 0) {
 		// Output data of each row
-		while($row = $result->fetch_assoc()) {
+		while($row = $results -> fetch_assoc()) {
 			echo "id: " . $row["id"]. " - Name: " . $row["title"]. " " . $row["short_description"]. "<br />";
 		}
 	} else {
 		echo "0 results";
 	}
 	
-	$conn->close();
+	$connection -> close();
 ?>
