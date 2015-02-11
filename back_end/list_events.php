efa1cae460e9a805f50a99eda70f06f63e93996f
Léo Davesne <leo@davesne.net>
2015-02-10 23:04:40 -0500

    Page de base pour lister les events

--- back_end/list_events.php
+++ back_end/list_events.php
@@ -1,26 +1,28 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+	$server_name = "127.0.0.1";
+	$username = "root";
+	$password = "";
+	$db_name = "acc";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	// Create connection
+	$conn = new mysqli($server_name, $username, $password, $db_name);
+	
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
 
-$sql = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
-$result = $conn->query($sql);
+	$sql = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
+	$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+	if ($result->num_rows > 0) {
+		// Output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["id"]. " - Name: " . $row["title"]. " " . $row["short_description"]. "<br />";
+		}
+	} else {
+		echo "0 results";
+	}
+	
+	$conn->close();
 ?>
