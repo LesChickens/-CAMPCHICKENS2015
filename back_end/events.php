96327649a9ccb5cf2df84e00df086c850ae1039f
Léo Davesne <leo@davesne.net>
2015-03-29 19:52:52 -0400

    Ajout du fichier de configuration, page des events

--- back_end/events.php
+++ back_end/events.php
@@ -1,4 +1,6 @@
 <?php
+	header('Content-Type: text/html; charset=utf-8');
+
 	$server_name = "127.0.0.1";
 	$username = "root";
 	$password = "";
@@ -6,11 +8,13 @@
 
 	// Create connection
 	$connection = new mysqli($server_name, $username, $password, $db_name);
+
+	mysqli_set_charset($connection, "utf8");
 	
 	// Check connection
 	if ($connection -> connect_error) {
 		die("Connection failed: " . $connection -> connect_error);
-	} 
+	}
 
 	$select_events = "SELECT id, title, short_description, long_description, start_date, end_date, author, category, level FROM events";
 	$results = $connection -> query($select_events);
@@ -20,18 +24,19 @@
 	if ($results -> num_rows > 0) {
 		// Output data of each row
 		while($row = $results -> fetch_assoc()) {
-			echo "id: " . $row["id"] . " title: " . $row["title"] . " short_description: " . $row["short_description"] . " long_description: " . $row["long_description"] . " start_date: " . $row["start_date"] . " end_date: " . $row["end_date"] . " author: " . $row["author"] . " category: " . $row["category"] . " level " . $row["level"] . "<br />";
+			//echo "id: " . $row["id"] . " title: " . $row["title"] . " short_description: " . $row["short_description"] . " long_description: " . $row["long_description"] . " start_date: " . $row["start_date"] . " end_date: " . $row["end_date"] . " author: " . $row["author"] . " category: " . $row["category"] . " level " . $row["level"] . "<br />";
 			
 			$row_array['id'] = $row['id'];
+			$row_array['title'] = $row['title'];
 			$row_array['short_description'] = $row['short_description'];
-            //$row_array['long_description'] = $row['long_description'];
-            //$row_array['start_date'] = $row['start_date'];
-            //$row_array['end_date'] = $row['end_date'];
-            //$row_array['author'] = $row['author'];
-            //$row_array['category'] = $row['category'];
-            //$row_array['level'] = $row['level'];
-			
-			array_push($arr,$row_array);
+			$row_array['long_description'] = $row['long_description'];
+			$row_array['start_date'] = $row['start_date'];
+			$row_array['end_date'] = $row['end_date'];
+			$row_array['author'] = $row['author'];
+			$row_array['category'] = $row['category'];
+			$row_array['level'] = $row['level'];
+
+			array_push($arr, $row_array);
 		}
 		
 		echo json_encode($arr);
