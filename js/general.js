04a1d48b528ec7a3ae4a1fd1784afadd23bfe1f0
Léo Davesne <leo@davesne.net>
2015-05-07 09:09:23 -0400

    Tuning configuration, splash screen

--- js/general.js
+++ js/general.js
@@ -1,3 +1,11 @@
+// Voir la splash screen plus longtemps
+document.addEventListener("deviceready", function (e) {
+	window.setTimeout(function () {
+		navigator.splashscreen.hide();
+	}, 5000);
+
+}, "false");
+
 // Url vers le contrôleur PHP principal
 ////var url="back_end/controller/controller.php";
 ////var url = "http://share-your-knowledge.olympe.in/Camp-Chickens-2015/back_end/controller/controller.php";
